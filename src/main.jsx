22fba2955596bc08e5bb416f36c95f63e128d615
Oozyman2 <abdulhammedoluwashina@gmail.com>
2025-02-25 15:43:08 -0800

    bootstrap resolved

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import 'bootstrap/dist/css/bootstrap.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ProductsProvider } from './context/Productcontext.jsx';
 import { AuthProvider } from './context/Authcontext.jsx';
